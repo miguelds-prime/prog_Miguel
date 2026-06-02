7ccb9ad5a30a7731108cddd8468553d1a8cb6e1f
miguelds-prime <miguel.dimitrius.silva@escola.pr.gov.br>
2026-06-02 11:48:22 +0000

    mais um jogo, mais um jogo

--- Lista/Lista01/A2.c
+++ Lista/Lista01/A2.c
@@ -9,10 +9,19 @@ struct Placar {
 
 int main () {
     struct Placar times;
-    scanf("%s", times.t1);
+    scanf("%49s", times.t1);
     scanf("%d", times.gt1);
-    scanf("%s", times.t1);
+    scanf("%49s", times.t1);
     scanf("%d", times.gt1);
 
-    printf ("-----PLACAR-----\n%s: %d X %s: %d\n", times.t1,)
+    printf ("-----PLACAR-----\n%s: %d X %s: %d\n", times.t1,times.gt1, times.t2,times.gt2 );
+
+    if (times.gt1 > times.gt2) {
+        printf ("%s Ganhou/n", times.t1);
+    } else if (times.gt2 > times.gt1){
+        printf ("%s Ganhou/n", times.t2);
+    } else if (times.gt2 == times.gt1){
+        printf ("EMPATOU/n");
+        }
+        return 0;
 }
